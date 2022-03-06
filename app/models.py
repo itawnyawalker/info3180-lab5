f864786f9e02e304bd846163cc0f5c28087461e8
Itawnya Walker <itawnya.walker@gmail.com>
2022-03-05 21:35:40 -0500

    Steps 1-5 Complete

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 
 
 class UserProfile(db.Model):
+    """ User profile"""
     # You can use this to change the table name. The default convention is to use
     # the class name. In this case a class name of UserProfile would create a
     # user_profile (singular) table, but if we specify __tablename__ we can change it
